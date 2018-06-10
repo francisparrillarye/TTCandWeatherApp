dba179727427ad6584d6b13b065214fd25ba94c0
francisparrillarye <francis.parrilla@ryeson.ca>
2018-06-09 23:31:38 -0400

    basic functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { NavController, List } from 'ionic-angular';
 import { WeatherProvider } from '../../providers/weather/weather';
 import { BusProvider } from '../../providers/bus/bus';
+//import { Refresher }
 
 @Component({
   selector: 'page-home',
@@ -53,6 +54,35 @@ export class HomePage {
       this.weather= this.weather.current_observation;
       console.log(this.weather); 
     });
+  }
+  refresh(){
+    this.bus = {
+      route:'113',
+      stopId: '3612'
+    }
+    this.location = {
+      city:'Toronto',
+      province:'ontario'
+    }
+    this.busProv.getSchedule(this.bus.stopId, this.bus.route).subscribe(bus => { 
+      console.log(bus);
+      this.pred = bus[0];
+      console.log(this.pred);
+      this.vals = bus[0].values;
+      for(var i = 0; i<bus[0].values.length; i++){
+          this.vals[i].minutes = Math.floor(bus[0].values[i].seconds / 60);
+          this.vals[i].seconds = bus[0].values[i].seconds % 60;
+      }
+     
+    });
+
+    this.weatherProv.getWeather(this.location.province, this.location.city).subscribe(weather => {
+      console.log(weather); 
+      this.weather = weather;
+      this.weather= this.weather.current_observation;
+      console.log(this.weather); 
+    });
+    complete();
   }
 
 }
